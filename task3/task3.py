ed888350dac205a171a66755ce757cf5d1cb22b1
Sekuoja <46537112+Sekuoja@users.noreply.github.com>
2021-12-20 18:29:45 +0300

    Update task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -1,1 +1,22 @@
+import json
+with open("tests.json") as f:
+    tests = json.load(f)
+    f.close()
 
+with open("values.json") as f:
+    values = json.load(f)
+    f.close()
+
+d={}
+for x in values['values']:
+    d[x['id']] = x['value']
+
+def set_value(sp):
+    sp['value'] = d[sp['id']]
+    if sp.setdefault("values", False):
+        for x in sp['values']:
+            x = set_value(x)
+    return sp
+
+with open('report.json', 'w') as outfile:
+    json.dump(set_value(tests), outfile)
