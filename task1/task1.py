f803c2dd5f55e95ec4e01e84805bd3f0696ed172
Sekuoja <46537112+Sekuoja@users.noreply.github.com>
2021-12-20 16:37:03 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,1 +1,19 @@
+import sys
 
+file = sys.argv[1]
+with open(file) as f:
+    n = int(f.readline())
+    m = int(f.readline())
+
+
+a = "".join([str(x) for x in range(1, m+1)])
+
+step = n-1
+rez = "1"
+while True:
+        if a[step % m] not in rez:
+            rez += a[step % m]
+        else:
+            break
+        step += n-1
+print(rez)
