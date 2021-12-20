52ac283a0c933d9f66ce27a2a5a57976e226ae48
Sekuoja <46537112+Sekuoja@users.noreply.github.com>
2021-12-20 17:03:18 +0300

    Update task4.py

--- task4/task4.py
+++ task4/task4.py
@@ -1,1 +1,9 @@
+import sys
 
+file = sys.argv[1]
+with open(file) as f:
+    n = [int(x.rstrip()) for x in f.readlines()]
+    f.close()
+sred = round(sum(n) / len(n))
+
+print(sum([abs(x - sred) for x in n]))
